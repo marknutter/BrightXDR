e3f961236fb2a5437b149643b98dd2ed353a93b7
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2025-05-02 15:56:24 +0300

    Add menu bar item

--- BrightXDR/main.swift
+++ BrightXDR/main.swift
@@ -16,6 +16,31 @@ let delegate = AppDelegate()
 // Link the delegate to app
 app.delegate = delegate
 
+// Create a status item in the menu bar
+let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+// Create an NSMenu
+let menu = NSMenu(title: "BrightXDR")
+
+// Add the application name to the menu
+let appNameItem = NSMenuItem(title: "BrightXDR", action: nil, keyEquivalent: "")
+appNameItem.isEnabled = false
+menu.addItem(appNameItem)
+
+// Add a separator
+menu.addItem(NSMenuItem.separator())
+
+// Add a Quit item to the menu
+menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+// Assign the menu to the status item
+statusItem.menu = menu
+
+// Set a default system icon for the status item
+if let button = statusItem.button {
+    button.image = NSImage(systemSymbolName: "sun.max.fill", accessibilityDescription: "Brightness")
+}
+
 // Load the Objective-C runtime and initialize the AppKit framework
 _ = __NSApplicationLoad()
 
